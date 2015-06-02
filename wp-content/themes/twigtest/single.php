8ef700926c75ce0958e194e22659e1f0c467623b
root <mrosprey@gmail.com>
2015-06-02 16:50:02 -0400

    Add header and footer to single.php

--- wp-content/themes/twigtest/single.php
+++ wp-content/themes/twigtest/single.php
@@ -1,6 +1,16 @@
 <?php
-$context = Timber::get_context();
-$context['post'] = new TimberPost(); // It's a new TimberPost object, but an existing post from WordPress.
+get_header(); ?>
 
-//var_dump($context);
-Timber::render('templates/single.twig', $context);
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+		<?php
+		$context = Timber::get_context();
+		$context['post'] = new TimberPost(); // It's a new TimberPost object, but an existing post from WordPress.
+
+		//var_dump($context);
+		Timber::render('templates/single.twig', $context);
+		?>
+    </div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
+
+<?php get_footer(); ?>
