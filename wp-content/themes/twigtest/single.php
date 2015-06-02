45b5bc905c8f1b0ddd17c6147df74e39b6470963
Toyin Akinmusuru <mrosprey@gmail.com>
2015-06-02 16:50:44 -0500

    update to old single template

--- wp-content/themes/twigtest/single.php
+++ wp-content/themes/twigtest/single.php
@@ -1,16 +1,48 @@
 <?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
 get_header(); ?>
 
-<div id="primary" class="content-area">
-    <div id="content" class="site-content" role="main">
-		<?php
-		$context = Timber::get_context();
-		$context['post'] = new TimberPost(); // It's a new TimberPost object, but an existing post from WordPress.
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-		//var_dump($context);
-		Timber::render('templates/single.twig', $context);
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			/*
+			 * Include the post format-specific template for the content. If you want to
+			 * use this in a child theme, then include a file called called content-___.php
+			 * (where ___ is the post format) and that will be used instead.
+			 */
+			get_template_part( 'content', get_post_format() );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+			// Previous/next post navigation.
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
+
+		// End the loop.
+		endwhile;
 		?>
-    </div><!-- #content .site-content -->
-</div><!-- #primary .content-area -->
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
